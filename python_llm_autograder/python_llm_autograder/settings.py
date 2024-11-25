cffca4809b9ce2980f8330183bfbef19b62c568d
Shubhasheesh-2020 <ashuprakash@gmail.com>
2024-11-25 19:30:38 +0800

    change settings.py

--- python_llm_autograder/python_llm_autograder/settings.py
+++ python_llm_autograder/python_llm_autograder/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
