525f6e125ae7fe05ab711fe2a7b50bf253ac4252
Shubhasheesh-2020 <ashuprakash@gmail.com>
2024-11-25 20:21:05 +0800

    add db

--- python_llm_autograder/python_llm_autograder/settings.py
+++ python_llm_autograder/python_llm_autograder/settings.py
@@ -119,7 +119,7 @@ WSGI_APPLICATION = "python_llm_autograder.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "autograder.sqlite3",
         # "ENGINE": "django.db.backends.mysql",
         # "NAME": "autograder_job",
         # "USER": "root",
